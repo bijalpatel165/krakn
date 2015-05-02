49aae55db5ad10666d487786280ee0ee0f7e8daf
Sudhanva <Sudhanva@Sudhanva.local>
2015-05-01 20:20:50 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 01kraknApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 01kraknApp
+ */
+angular.module('01kraknApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 01kraknApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the 01kraknApp
+ */
+angular.module('01kraknApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
